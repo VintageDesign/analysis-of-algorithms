850068bd87d2650050d0856c28f08324988bcba0
Riley Kopp <koppril7@gmail.com>
2019-08-28 08:49:58 -0600

    Added more tests

--- 1_primary/work/hw1_starter.cpp
+++ 1_primary/work/hw1_starter.cpp
@@ -4,10 +4,31 @@
 #include <time.h>
 #include <vector>
 #include <chrono>
+#include <algorithm>
+#include <string>
 
 #include "quickSort.h"
 using namespace std;
 
+void printVector(vector<int> sortResult)
+{
+
+    for (int val : sortResult)
+    {
+       cout << val << ", ";
+    }
+    cout << endl;
+    return;
+}
+
+
+void checkSort(vector<int> sortedResult, vector<int> original)
+{
+    sort(original.begin(), original.end());
+
+    string passFlag =  original == sortedResult ? "Pass" : "Fail";
+    cout << passFlag << endl;
+}
 
 /******************************************************
 Template function for running correctness tests
@@ -22,14 +43,9 @@ void correctness() {
     cout << "Test 1: Basic sort with 2 elements" << endl;
     cout << "Original: 2, 1" << endl;
     cout << "Quick Result: ";
-    //TODO test, and display result
     vector<int> quickSortResult = quickSort(test1, 0, 1);
-
-    for (int val : quickSortResult)
-    {
-       cout << val << " ";
-    }
-    cout << endl;
+    printVector(quickSortResult);
+    checkSort(quickSortResult, test1);
     cout << "Heap Result: ";
     //TODO test, and display result
 
@@ -38,7 +54,9 @@ void correctness() {
     cout << "Test 2: Basic sort with 3 elements" << endl;
     cout << "Original: 1, 3, 2" << endl;
     cout << "Quick Result: ";
-    //TODO test, and display result
+    quickSortResult = quickSort(test2, 0, 2);
+    printVector(quickSortResult);
+    checkSort(quickSortResult, test2);
     cout << "Heap Result: ";
 
     cout << endl;
@@ -46,7 +64,9 @@ void correctness() {
     cout << "Test 3: Basic sort with even # of elements" << endl;
     cout << "Original: 2, 2, 1, 3, 5, 4, 8, 0 " << endl;
     cout << "Quick Result: ";
-    //TODO test, and display result
+    quickSortResult = quickSort(test3, 0, 7);
+    printVector(quickSortResult);
+    checkSort(quickSortResult, test3);
     cout << "Heap Result: ";
 
     cout << endl;
