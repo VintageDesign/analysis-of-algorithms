b77a4be7ac1b1d44bcc2fc278107ab4ddf6ac28e
Riley Kopp <riley.kopp@mines.sdsmt.edu>
2019-10-17 07:27:08 -0600

    Added pathing

--- 2_primary/DFS_Solution.cpp
+++ 2_primary/DFS_Solution.cpp
@@ -25,19 +25,22 @@ class GraphSolver{
       int start;
       double **path;
       vector<struct Edge> edges;
-      vector<vector<int>> finalPath;
+      int **finalPath;
 
 
       GraphSolver(int sizeIn, vector<struct Edge> edgeIn, int maxK,int startIn, int endIn)
       {
          path = new double*[maxK+1];
+         finalPath = new int*[maxK+1];
 
          for( int i = 0; i < maxK+1; i++)
          {
             path[i] = new double[sizeIn];
+            finalPath[i] = new int[sizeIn];
             for (int j = 0; j < sizeIn; j++)
             {
                path[i][j] = DBL_MAX;
+               finalPath[i][j] = -1;
             }
          }
 
@@ -48,7 +51,6 @@ class GraphSolver{
          end = endIn;
          start = startIn;
 
-         finalPath = vector<vector<int>> (edgeIn.size(), vector<int>(0));
 
       }
 
@@ -58,10 +60,6 @@ class GraphSolver{
          int s = 0;
          int t = 0;
 
-         for (int i = 0; i < finalPath.size(); i++)
-         {
-            finalPath[i].push_back(start);
-         }
 
          for(int kIndex = 1; kIndex < k+1; kIndex++)
          {
@@ -72,8 +70,10 @@ class GraphSolver{
                if( path[kIndex][t] > min(path[kIndex-1][t],path[kIndex-1][s] + edges[i].cost))
                {
                   path[kIndex][t] = min( path[kIndex][t], min(path[kIndex-1][t],path[kIndex-1][s] + edges[i].cost));
-                  finalPath[i].pop_back();
-                  finalPath[i].push_back(t);
+                  if(path[kIndex][t] == path[kIndex-1][t])
+                     finalPath[kIndex][t] = finalPath[kIndex-1][t];
+                  else
+                     finalPath[kIndex][t] = s;
 
                }
                t = edges[i].from;
@@ -81,8 +81,10 @@ class GraphSolver{
                if( path[kIndex][t] > min(path[kIndex-1][t],path[kIndex-1][s] + edges[i].cost))
                {
                   path[kIndex][t] = min( path[kIndex][t], min(path[kIndex-1][t],path[kIndex-1][s] + edges[i].cost));
-                  finalPath[i].pop_back();
-                  finalPath[i].push_back(t);
+                  if(path[kIndex][t] == path[kIndex-1][t])
+                     finalPath[kIndex][t] = finalPath[kIndex-1][t];
+                  else
+                     finalPath[kIndex][t] = s;
                }
             }
          }
@@ -99,7 +101,7 @@ vector<struct Edge> read_file(string file_name, int& size){
    double t;
    int counter = 0;
    inFile.open(file_name);
-// Written by Ayden
+   // Written by Ayden
    if (!inFile) {
       cout << "Unable to open file";
       exit(1); // terminate with error
@@ -113,7 +115,7 @@ vector<struct Edge> read_file(string file_name, int& size){
       counter +=1;
    }
    inFile.close();
-// End of Ayden Code
+   // End of Ayden Code
    for(int i = 0; i < size; i++)
    {
       for(int j = i + 1; j < size; j++)
@@ -134,44 +136,65 @@ vector<struct Edge> read_file(string file_name, int& size){
 
 int main(int argc, char* argv[])
 {
-   // input comes in like `inputGraph start end k`
-   vector<struct Edge> graph;
-   int start = atoi(argv[2]) - 1; // All graphs inside the Solver are 0 indexed
-   int end = atoi(argv[3]) - 1;
-   int k = atoi(argv[4]);
-   int size;
-   double ** final;
-
-   graph = read_file(argv[1], size);
-
-   /*
-   for(auto e: graph)
+   cout << "===========================================================" << endl;
+   for(int iter = 0; iter < (argc-1) /4 ; iter++)
    {
-      cout << "V<f,t>: " << e.to << " " << e.from
-         << " Cost: " << e.cost << endl;
-   }
-   */
+      cout << "Test " << iter + 1 << endl;
+      // input comes in like `inputGraph start end k`
+      vector<struct Edge> graph;
+      int start = atoi(argv[iter*4 + 2]) - 1; // All graphs inside the Solver are 0 indexed
+      int end = atoi(argv[iter*4 + 3]) - 1;
+      int k = atoi(argv[iter*4 + 4]);
+      int size;
+      double ** final;
+      vector<int> pathStack;
+      int buildK;
 
+      graph = read_file(argv[iter*4 + 1], size);
 
-   GraphSolver test(size, graph, k, start, end);
-   final = test.Solve();
+      GraphSolver test(size, graph, k, start, end);
+      final = test.Solve();
 
-   cout << "Cost: " << final[k][end] << endl;
-
-   for (int j = 0; j <= k; j++)
-   {
-      for(int i = 0; i < test.finalPath[j].size(); i++)
+      // Path
+      // the worst possible way to do this but.... Im still gonna send it.
+      buildK = k;
+      pathStack.push_back(end);
+      while (pathStack.back() != start)
       {
-         cout << test.finalPath[j][i] +1 << " ";
+         pathStack.push_back(test.finalPath[buildK][pathStack.back()]);
+         buildK--;
+      }
+
+      for(int i = 0; i < k +1; i++)
+      {
+         for(int j = 0; j < size; j++)
+         {
+            cout << test.finalPath[i][j]  + 1<< " ";
+         }
+         cout << endl;
+      }
+
+      buildK = 0;
+      cout << "Path: ";
+      if (final[k][end] == DBL_MAX)
+      {
+         cout << "None" << endl;
+      }
+      else
+      {
+         while(pathStack.size() != 1)
+         {
+            cout << pathStack.back() + 1 << " to ";
+            pathStack.pop_back();
+            buildK++;
+         }
+
+         cout << pathStack.back() + 1<< endl;
+         //cost
+         cout << "Cost: " << final[k][end] << endl;
+         cout << "===========================================================" << endl;
       }
    }
-   cout << endl;
-
-
-
-
-
-
 }
 
 
