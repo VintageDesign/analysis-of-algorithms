d0ea58111587419c723d3911d9a251161f5d2d34
Riley Kopp <koppril7@gmail.com>
2019-11-08 10:47:30 -0700

    Added segment intersect function

--- 3_primary/polygon.cpp
+++ 3_primary/polygon.cpp
@@ -3,6 +3,32 @@
 #include <vector>
 #include <utility>
 
+bool segementsIntersect(std::pair<double, double> p1,
+                        std::pair<double, double> p2,
+                        std::pair<double, double> p3,
+                        std::pair<double, double> p4)
+{
+   double d1 = direction(p3, p4, p1);
+   double d2 = direction(p3, p4, p2);
+   double d3 = direction(p1, p2, p3);
+   double d4 = direction(p1, p2, p4);
+   bool retval = false;
+
+   if( ((d1 > 0 && d2 < 0) || (d1 < 0 && d2 > 0))
+    && ((d3 > 0 && d4 < 0) || (d3 < 0 && d4 > 0)))
+      retval = true;
+   else if ( d1 == 0 && onSegment(p3, p4, p1))
+      retval = true;
+   else if ( d2 == 0 && onSegment(p3, p4, p2))
+      retval = true;
+   else if ( d3 == 0 && onSegment(p1, p2, p3))
+      retval = true;
+   else if ( d4 == 0 && onSegment(p1, p2, p4))
+      retval = true;
+
+   return retval;
+}
+
 int main(void)
 {
    //TODO change this to cmd input
